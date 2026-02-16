db5bd5fc0f69f2d68ff5603ef5ac5b1a6bfb9dc0
Tepen1s <stevenwan07@gmail.com>
2026-02-16 15:23:41 +0700

    week01:(tugas 2) implement discount logic

--- src/oop_00000077033_stevenwan/week01/GameStore.kt
+++ src/oop_00000077033_stevenwan/week01/GameStore.kt
@@ -1,2 +1,24 @@
 package oop_00000077033_stevenwan.week01
 
+fun main() {
+    val gameTitle: String = "Cyberpunk 2077"
+    val price: Int = 700000
+
+    val finalPrice = calculateDiscount(price)
+
+    displayReceipt(gameTitle, price, finalPrice)
+}
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) {
+    (price * 0.8).toInt() // Diskon 20%
+} else {
+    (price * 0.9).toInt() // Diskon 10%
+}
+
+fun displayReceipt(title: String, originalPrice: Int, total: Int) {
+    println("=== Struk Kasir SteamKW ===")
+    println("Judul Game  : $title")
+    println("Harga Asli  : Rp $originalPrice")
+    println("Harga Akhir : Rp $total")
+    println("===========================")
+}
