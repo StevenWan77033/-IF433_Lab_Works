ca8c679a3b86564c122b3be2001779dc65d0f76e
Tepen1s <stevenwan07@gmail.com>
2026-02-16 17:59:28 +0700

    week02: create basic Student class structure

--- src/oop_00000077033_stevenwan/week02/Student.kt
+++ src/oop_00000077033_stevenwan/week02/Student.kt
@@ -1,42 +1,9 @@
 package oop_00000077033_stevenwan.week02
 
-import java.util.Scanner
-
-fun main() {
-    val scanner = Scanner(System.`in`)
-
-    println("--- APLIKASI PMB UMN ---")
-
-    print("Masukkan Nama: ")
-    val name = scanner.nextLine()
-
-    print("Masukkan NIM (Wajib 5 Karakter): ")
-    val nim = scanner.next()
-
-    scanner.nextLine()
-
-    if (nim.length != 5) {
-        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-    } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
-
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
-    }
-}
-
-class Student (
+class Student(
     val name: String,
     val nim: String,
-    var major: String,
+    var major: String
 ){
-    init {
-        if (nim.length != 5) {
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-        } else {
-            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
-        }
-    }
+
 }
