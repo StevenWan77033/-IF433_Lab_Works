e484865b5a884471939af15a96305374876b4d84
Tepen1s <stevenwan07@gmail.com>
2026-04-11 23:14:51 +0700

    week02: create basic Student class structure

--- src/oop_00000077033_stevenwan/week02/Student.kt
+++ src/oop_00000077033_stevenwan/week02/Student.kt
@@ -1,16 +1,9 @@
 package oop_00000077033_stevenwan.week02
 
-class Student(
+class Student (
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
 ){
-    init {
-        if (nim.length != 5) {
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-        } else {
-            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
-        }
-    }
+
 }
