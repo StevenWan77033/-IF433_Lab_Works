1d3a217ba445ce2523ebd38b517d3949698cb9b5
Tepen1s <stevenwan07@gmail.com>
2026-04-13 15:58:18 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_00000077033_stevenwan/Week05/PaymentMain.kt
+++ src/oop_00000077033_stevenwan/Week05/PaymentMain.kt
@@ -3,13 +3,23 @@ package oop_00000077033_stevenwan.Week05
 fun main() {
     val myWallet = EWallet("Dana John", 50000.0)
     val myCard = CreditCard("Visa John", 100000.0)
-
     val daftarPembayaran: List<PaymentMethod> = listOf(myWallet, myCard)
 
     println("=== PROSES TRANSAKSI RP 75.000 ===")
 
     for (metode in daftarPembayaran) {
         metode.processPayment(75000.0)
+
+        if (metode is EWallet) {
+            println("-> [Recovery] Saldo E-Wallet mungkin kurang. Melakukan top up otomatis...")
+
+            metode.topUp(50000.0)
+
+            println("-> Mencoba pembayaran ulang...")
+            metode.processPayment(75000.0)
+        }
+
         println("-----------------------------------")
     }
 }
+
