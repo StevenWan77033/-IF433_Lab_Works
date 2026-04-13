f304bdd7ec08502457e6d7ee4dde50e7874bcc1d
Tepen1s <stevenwan07@gmail.com>
2026-04-13 15:27:03 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_00000077033_stevenwan/week04/Employee.kt
@@ -0,0 +1,12 @@
+package oop_00000077033_stevenwan.week04
+
+open class Employee(val name: String, val baseSalary: Int) {
+
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return (0.1 * baseSalary).toInt()
+    }
+}
