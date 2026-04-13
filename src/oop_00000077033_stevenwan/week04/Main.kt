7334eb99632c436529f3af0d23a7dbbfeab58258
Tepen1s <stevenwan07@gmail.com>
2026-04-13 15:31:01 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000077033_stevenwan/week04/Main.kt
+++ src/oop_00000077033_stevenwan/week04/Main.kt
@@ -1,6 +1,19 @@
 package oop_00000077033_stevenwan.week04
 
 fun main() {
+    val boss = Manager("Budi", 10000000)
+    val coder = Developer("Aris", 8000000, "Kotlin")
+
+    println("--- Data Manager ---")
+    boss.work()
+    println("Bonus: Rp${boss.calculateBonus()}")
+
+    println("\n--- Data Developer ---")
+    coder.work()
+    println("Bonus: Rp${coder.calculateBonus()}")
+
+    println("   ")
+
     println("--- Testing Vehicle ---")
     val generalVehicle = Vehicle(brand = "Sepeda Onthel")
     generalVehicle.honk()
