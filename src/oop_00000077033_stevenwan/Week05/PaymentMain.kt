bf1e9e6a5c503959651bc98fee7c243d85d6a385
Tepen1s <stevenwan07@gmail.com>
2026-04-13 15:56:33 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000077033_stevenwan/Week05/PaymentMain.kt
@@ -0,0 +1,15 @@
+package oop_00000077033_stevenwan.Week05
+
+fun main() {
+    val myWallet = EWallet("Dana John", 50000.0)
+    val myCard = CreditCard("Visa John", 100000.0)
+
+    val daftarPembayaran: List<PaymentMethod> = listOf(myWallet, myCard)
+
+    println("=== PROSES TRANSAKSI RP 75.000 ===")
+
+    for (metode in daftarPembayaran) {
+        metode.processPayment(75000.0)
+        println("-----------------------------------")
+    }
+}
