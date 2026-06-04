144f21001c0affcd15dc020d2223c39a6d824081
Steven Wan <kerjaantepen@gmail.com>
2026-06-04 18:35:36 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000077033_stevenwan/week06/MainSmartHome.kt
+++ src/oop_00000077033_stevenwan/week06/MainSmartHome.kt
@@ -4,4 +4,15 @@ fun main() {
     val lamp = SmartLamp(id = "L01", name = "Ruang Tamu")
     val speaker = SmartSpeaker(id = "S01", name = "Google Nest Dapur")
     val cctv = SmartCCTV(id = "C01", name = "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+
+    println("=== MENDAFTARKAN PERANGKAT ===")
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    // Pengujian fungsi logika inti Smart Home
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
